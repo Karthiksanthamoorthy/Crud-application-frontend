b5af4eb0778d1b6ac1961d1db4583419dd9c2f35
karthiksanthamoorthy <karthiksanthamoorthy713@gmail.com>
2023-03-26 08:19:01 +0530

    app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Register from './components/Register';
 import Edit from './components/Edit';
 import Details from './components/Details'
-import {BrowserRouter as Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
